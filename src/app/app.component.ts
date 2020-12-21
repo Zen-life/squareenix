d37e69d7d7f22dc3233d8d29058adfe106f691ec
C. Anim <hwe_ha@ymail.com>
2020-12-21 18:27:46 +0000

    changing errors to alert rather than log via console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,14 +19,12 @@ export class AppComponent implements OnInit {
     .subscribe((response: any) => {
       console.log('res: ', response);
       this.games.splice(0, 0, response);
-      let game = this.games
-      console.log('games: ',game)
     }, (error: Response) => {
       if (error.status === 404) {
-        console.log('Something went wrong, we cannot find the page');
+       alert('Something went wrong, we cannot find the page');
       } else {
-        console.log('An unexpected error occurred');
-        console.log(error); 
+        alert('An unexpected error occurred');
+        // console.log(error); 
       }
       
     })
